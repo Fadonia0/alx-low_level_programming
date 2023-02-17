fcc23c8ba9a4f57c11856e8f72a513241b0966bc
Fadonia0 <smuhatia>
2023-02-16 17:00:19 -0800

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+1 #include <stdio.h>
+2 /**
+3 *main - entry point
+4 *
+5 *Return: returns 0
+6 */
+7 
+8 int main(void)
+9 {
+10         puts("\programming is like building a multilingual puzzle");
+11         return (0);
