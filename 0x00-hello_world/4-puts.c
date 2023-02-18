a3369c3c90507f22646f76c2acb9023a4a6f80df
Fadonia0 <smuhatia>
2023-02-18 11:34:54 -0800

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
+
 #include <stdio.h>
+
 /**
-*main - entry point
-*
-*Return: returns 0
-*/
- 
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-        puts("\programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
+
